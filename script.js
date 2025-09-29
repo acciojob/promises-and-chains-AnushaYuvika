8337947ba038c51d5298fbdbd55093c28fde4f24
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2025-09-29 23:03:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const ageInput = document.getElementById("age");
+const nameInput = document.getElementById("name");
+const btn = document.getElementById("btn");
+
+function checkVotingEligibility(age, name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000); 
+  });
+}
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault(); 
+
+  const age = ageInput.value.trim();
+  const name = nameInput.value.trim();
+
+  if (!age || !name) {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  checkVotingEligibility(Number(age), name)
+    .then((msg) => alert(msg))
+    .catch((msg) => alert(msg));
+});
